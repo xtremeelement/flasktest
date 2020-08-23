8a0cc6f4e80f3b639db1dc2a07a50a1c48b8ad2b
Brandon Downhour <brandon@Brandons-MacBook-Pro.local>
2020-08-23 10:21:12 -0400

    Switched to mysql db

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/postdb'
 
 db = SQLAlchemy(app)
 
