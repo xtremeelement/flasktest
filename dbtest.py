6ecc9456abf9e953f4e778329ed30e231582c692
Brandon Downhour <brandon@Brandons-MacBook-Pro.local>
2020-08-23 13:58:52 -0400

    fixed query error

--- dbtest.py
+++ dbtest.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from datetime import datetime
 
 db = mysql.connector.connect(
     host="localhost",
@@ -9,10 +10,11 @@ db = mysql.connector.connect(
 cursor = db.cursor(dictionary=True)
 
 def getAll():
-    cursor.execute("SELECT * FROM blog_post")
+    cursor.execute("SELECT * FROM blog_post ORDER BY created_at DESC;")
     result = cursor.fetchall()
     return result
     
 def create(title, content, author):
-    cursor.execute("INSERT INTO blog_post(title,content,author) VALUES(%s, %s, %s)",(title, content, author))
+    date = datetime.utcnow()
+    cursor.execute("INSERT INTO blog_post(title,content,author, created_at) VALUES(%s, %s, %s, %s)",(title, content, author, date))
     db.commit()
