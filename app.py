dfbee0ee6628c46a8b394883d94e06a7688e2550
Brandon Downhour <brandon@Brandons-MacBook-Pro.local>
2020-08-23 00:13:36 -0400

    set up templates

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+myposts = [
+    {
+        'title': "post 1",
+        'content': 'loremjlj  lksjdf lkj alsjdf',
+        'author': 'testAuthor'
+    },
+    {
+        'title': 'post 2',
+        'content': 'lklka dkf as dflkaj sdf'
+    }
+]
 
 @app.route('/')
 def index():
-    return "<h1>hello world</h1>"
+    return render_template('index.html')
 
-@app.route('/home')
-def test():
-    return"hi"
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=myposts)
 
 if __name__ == '__main__':
     app.run(debug=True)
